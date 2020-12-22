f2ee1fd2d937388ceda388f58c5ea716df272b49
momo1093324 <71679878+momo1093324@users.noreply.github.com>
2020-12-22 12:00:07 +0800

    籃網分數碰撞器

---
+++ unity_asia_vr_basketball/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI; //引用介面API
+
+public class ScoreManager : MonoBehaviour
+{
+    [Header("介面分數")]
+    public Text textScore;
+    [Header("分數")]
+    public int score;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.name);
+    }
+}
